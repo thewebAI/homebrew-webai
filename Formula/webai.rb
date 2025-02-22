346b14dc0cc2c7d0d1d98f077fa968978c7323fc
Mitch DePree <mitch.depree@webai.com>
2025-02-21 19:26:08 -0500

    reorder execute

--- Formula/webai.rb
+++ Formula/webai.rb
@@ -20,8 +20,8 @@ class Webai < Formula
   sha256 "050367544c767eda8c7c284b2d5ede7a3a993132594c6fa1474d55d4160fe351"
 
   def install
-    bin.install "webai"
     chmod 0755, bin/"webai"
+    bin.install "webai"
   end
 
   test do
