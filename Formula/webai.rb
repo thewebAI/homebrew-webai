b2eaed79c711bf5f4d4c517aa3e7bef74c7fd7ea
Mitch DePree <mitch.depree@webai.com>
2025-02-24 14:29:36 -0500

    remove bottle code and add a url

--- Formula/webai.rb
+++ Formula/webai.rb
@@ -2,17 +2,8 @@ class Webai < Formula
   desc "WebAI CLI - developer entry point into the webAI platform"
   homepage "https://github.com/thewebAI/webai-cli"
   version "0.1.15"
-  license "MIT"  # Add license if applicable
-  
-  # Use `head` to allow updates from the main branch
-  head "https://github.com/thewebAI/webai-cli.git", branch: "main"
-
-  # Add a bottle block to improve installation speed (optional)
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ced22675a0b36d0051e3335faa145d360213c4a6d952603bc40f408184037a92"
-    sha256 cellar: :any_skip_relocation, monterey: "e231654d831d5c4207e0ea29f830300ef2c04c72209da7039434c62a94a51633"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5db917f7d50afdeca97ba5b02c78f6909952722bfbf50bae015c7f55bff1e7fa"
-  end
+  url "https://github.com/thewebAI/webai-cli/archive/refs/tags/v#{version}.tar.gz"
+  license "MIT"
 
   on_macos do
     on_arm do
