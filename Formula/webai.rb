b3f9d75c07ca97a5dc35704f51eb40c636f9b9d7
GitHub Action <action@github.com>
2025-02-24 18:35:59 +0000

    webai: update to v0.1.12

--- Formula/webai.rb
+++ Formula/webai.rb
@@ -6,17 +6,17 @@ class Webai < Formula
   on_macos do
     on_arm do
       url "https://github.com/thewebAI/webai-cli/releases/download/v0.1.12/webai"
-      sha256 ""
+      sha256 "a05b631b50f6442827c9f58cd06b3304640e7daec2d72f30d7186172b8872b1f  webai-darwin-arm64"
     end
     on_intel do
       url "https://github.com/thewebAI/webai-cli/releases/download/v0.1.12/webai"
-      sha256 ""
+      sha256 "2cf3de7809c20ddbe58033545ab331ca11f1663464e0f73ea19d9bcd9b22d87f  webai-darwin-x64"
     end
   end
 
   on_linux do
     url "https://github.com/thewebAI/webai-cli/releases/download/v0.1.12/webai"
-    sha256 ""
+    sha256 "6a90e6d3397d9169b35dac35ca5068fe446802bd0ac5e67935141ec15b3a9519  webai-linux"
   end
 
   def install
